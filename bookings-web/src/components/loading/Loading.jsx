4d9d2b488a0a0d9a658c5127f3e2c1b6338b3551
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-05-21 09:17:51 -0700

    Add Loading state and component

---
+++ bookings-web/src/components/loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+<div>
+    <img src={'https://cdn.dribbble.com/users/722246/screenshots/4400319/loading_crescor_dribbble.gif'} alt={'Loading'} />
+</div>
+    );
+};
+
+export default Loading;
--- bookings-web/src/containers/Getaways.jsx
+++ bookings-web/src/containers/Getaways.jsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { getPlaces } from '../services/placesApi';
 import PlaceList from '../components/places/PlaceList';
+import Loading from '../components/loading/Loading';
 
 const Getaways = () => {
+  const [loading, setLoading] = useState(true);
   const [places, setPlaces] = useState([]);
 
   useEffect(() => {
-    getPlaces().then(setPlaces);
+    getPlaces()
+    .then(setPlaces)
+    .finally(() => setLoading(false));
   }, []);
+
+  if(loading) return <Loading />;
 
   return <PlaceList places={places} />;
 };
